706a22a0f14de96b677b5f528d7e09e7e9f59a8e
lsrhazaki <126371949+lsrhazaki@users.noreply.github.com>
2024-06-07 21:02:44 +0800

    Add files via upload

---
+++ nixconf/fish/conf.d/variables.fish
@@ -0,0 +1,11 @@
+# set XDG Base Directory Specification - there could be a better way to do this
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_DESKTOP_DIR $HOME/Desktop
+set -gx XDG_DOWNLOAD_DIR $HOME/Downloads
+set -gx XDG_DOCUMENTS_DIR $HOME/Documents
+set -gx XDG_MUSIC_DIR $HOME/Music
+set -gx XDG_PICTURES_DIR $HOME/Images
+set -gx XDG_VIDEOS_DIR $HOME/Videos
+
---
+++ nixconf/fish/config.fish
@@ -0,0 +1,10 @@
+source ~/.config/fish/conf.d/abbr.fish
+source ~/.config/fish/conf.d/variables.fish
+source ~/.config/fish/conf.d/colors.fish # this could be obsolete by starship & eza
+
+
+#Enable FastFetch.
+fastfetch
+
+#Enable Starship.
+starship init fish | source
