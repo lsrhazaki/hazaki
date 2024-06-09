5322ad62548b8df2b09e7fc97e2828aed27cdd3e
lsrhazaki <126371949+lsrhazaki@users.noreply.github.com>
2024-06-09 20:46:07 +0800

    Delete arcolinux i3 my setup directory

--- arcolinux i3 my setup/polybar/scripts/pacman-updates.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-[[ $(cat /etc/os-release | grep arch) ]] && yay -Qu | wc -l
-#[[ $(cat /etc/os-release | grep debian) ]] && apt list --upgradeable | wc -l
-
-[[ $(cat /etc/os-release | grep debian) ]] && apt list --upgradable 2>/dev/null | wc -l
